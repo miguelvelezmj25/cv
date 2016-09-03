bb20a108417deabd4bf23e5168639e8bc4563126
Miguel Velez <miguelvelez@mijecu25.com>
2016-09-02 23:13:55 -0400

    Initial commit

---
+++ generateCV.sh
@@ -0,0 +1,12 @@
+echo "############################# Cleaning latex files #############################"
+latexmk -c Miguel\ Velez\ CV
+echo "\n############################# pdflatex  #############################" 
+pdflatex Miguel\ Velez\ CV
+echo "\n############################# biber #############################"
+biber Miguel\ Velez\ CV
+echo "\n############################# pdflatex #############################"
+pdflatex Miguel\ Velez\ CV
+echo "\n############################# pdflatex #############################"
+pdflatex Miguel\ Velez\ CV
+echo "\n############################# Cleaning latex files #############################"
+latexmk -c Miguel\ Velez\ CV
